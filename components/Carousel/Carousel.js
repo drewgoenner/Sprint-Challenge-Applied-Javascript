97e28ae67f6bd90f2917124432cfb616975c7f16
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2019-08-09 13:04:25 -0500

    started work on carousel, can't get it to show up

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,53 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+const caroCont = document.querySelector(".carousel-container");
+  caroCont.appendChild(carousel1);
+
+
+function createCar(info) {
+  //create elements
+  const carousel1 = document.createElement('div');
+  const leftButton1 = document.createElement('div');
+  const img1 = document.createElement('img');
+  const img2 = document.createElement('img');
+  const img3 = document.createElement('img');
+  const img4 = document.createElement('img');
+  const rightButton1 = document.createElement('div');
+
+  
+
+  //append elements
+  carousel1.appendChild(leftButton1);
+  carousel1.appendChild(img1);
+  carousel1.appendChild(img2);
+  carousel1.appendChild(img3);
+  carousel1.appendChild(img4);
+  carousel1.appendChild(rightButton1);
+
+  //add classes
+  carousel1.classList.add("carousel");
+  leftButton1.classList.add("left-button");
+  rightButton1.classList.add("right-button");
+
+  //add content
+  // carousel1.textContent = "Hi there";
+  leftButton1.textContent = "Left";
+  rightButton1.textContent = "Right";
+  img1.src = "./assets/carousel/computer.mountains.jpeg";
+  img2.src = "./assets/carousel/computer.jpeg";
+  img3.src = "./assets/carousel/trees.jpeg";
+  img4.src = "./assets/carousel/turntable.jpeg"
+
+  //add listener
+  leftButton1.addEventListener('click', (e) => {
+
+  })
+  const caroCont = document.querySelector(".carousel-container");
+  caroCont.appendChild(carousel1);
+  
+  return carousel1;
+
+
+}
